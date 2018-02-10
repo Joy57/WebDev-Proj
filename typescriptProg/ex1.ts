56452dfb89f8b4a52fe1e27a27747aec4340639c
Joy <abedinjoynal2@gmail.com>
2018-02-09 20:09:27 -0500

    adding typescript prac

---
+++ typescriptProg/ex1.ts
@@ -0,0 +1,21 @@
+// function log (message){
+//     console.log(message);
+// }
+
+// var message = 'hello there';
+// log (message);
+
+let message; 
+message = 'abc';
+let endsWitchC = (<string>message).endsWith('c'); //type assertion
+let alternativeWay = (message as string).endsWith('c'); //type assertion
+ 
+let log = function (msg){
+    console.log(msg);
+}
+let doLog = (msg) => console.log(msg); // array function
+
+
+let drawPoint = (x, y) =>{
+    //...
+}
