c158894b6504bb876051cc952e249b0f5a8859a3
Joy <abedinjoynal2@gmail.com>
2018-02-09 20:12:34 -0500

    added constructor in ex2

--- typescriptProg/ex2.ts
+++ typescriptProg/ex2.ts
@@ -1,6 +1,12 @@
 class Point{
     x: number;
     y: number;
+
+    constructor(x?: number, y?: number){
+        this.x = x;
+        this.y = y;
+    }
+
     draw(){
         console.log('X: '+ this.x + ', Y: '+ this.y);
     }
