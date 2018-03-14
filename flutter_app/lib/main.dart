0806ffb1d1b1bf38bdb28719e62a68ec62429980
Joy <abedinjoynal2@gmail.com>
2018-03-14 13:36:35 -0400

    updated message box

--- flutter_app/lib/main.dart
+++ flutter_app/lib/main.dart
@@ -1,4 +1,3 @@
-//import 'package:flutter/material.dart';
 import 'package:flutter/material.dart';
 
 void main(){
@@ -32,12 +31,26 @@ class ChatScreenState extends State<ChatScreen>{
   );
 }
   Widget _buildTextComposer(){
-    return new Container(
+    return new IconTheme(
+    data: new IconThemeData(color: Theme.of(context).accentColor),
+    child: new Container(
     margin: const EdgeInsets.symmetric(horizontal: 8.0),
-    child: new TextField(
-      controller: _textController,
-      onSubmitted: _handleSubmitted,
-      decoration: new InputDecoration.collapsed(hintText: "Type a message"),
+    child: new Row(
+     children: <Widget>[
+       new Flexible(
+           child: new TextField(
+             controller: _textController,
+             onSubmitted: _handleSubmitted,
+             decoration: new InputDecoration.collapsed(hintText: "Type a Message"),
+           ),
+       ),
+       new Container(
+         margin: new EdgeInsets.symmetric(horizontal: 4.0),
+         child: new IconButton(icon: new Icon(Icons.send),
+             onPressed: ()=> _handleSubmitted(_textController.text)),
+       ),
+     ],
+    ),
     ),
     );
   }
