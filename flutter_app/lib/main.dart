edcb9694e986faab8ab9e1893cb091dc94ba9732
Joy <abedinjoynal2@gmail.com>
2018-03-14 13:11:52 -0400

    flutter app update

--- flutter_app/lib/main.dart
+++ flutter_app/lib/main.dart
@@ -19,12 +19,30 @@ class ChatScreen extends StatefulWidget{
   State createState() => new ChatScreenState();
 }
 class ChatScreenState extends State<ChatScreen>{
+  //will  use it for reading the contents of the input field, and for clearing the field after the text message is sent.
+  //To manage interactions with the text field
+  final TextEditingController _textController = new TextEditingController();
   @override
   Widget build(BuildContext context){
   return new Scaffold(
     appBar: new AppBar(
       title: new Text("ChatBox"),
-    )
+    ),
+    body: _buildTextComposer(),
   );
 }
+  Widget _buildTextComposer(){
+    return new Container(
+    margin: const EdgeInsets.symmetric(horizontal: 8.0),
+    child: new TextField(
+      controller: _textController,
+      onSubmitted: _handleSubmitted,
+      decoration: new InputDecoration.collapsed(hintText: "Type a message"),
+    ),
+    );
+  }
+  void _handleSubmitted(String text){
+    _textController.clear();
+  }
 }
+
