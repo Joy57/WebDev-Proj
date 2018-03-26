afd5b5db97962240e116f907dbcb524b291fba8b
Joy <abedinjoynal2@gmail.com>
2018-03-26 00:10:31 -0400

    template

--- Angular/hello-world/src/app/courses.component.ts
+++ Angular/hello-world/src/app/courses.component.ts
@@ -2,9 +2,15 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'courses',
-    template: '<h2>Courses</h2>'//html markup to render in this component
+    template: '<h2>{{ getTitle() }}</h2>'//html markup to render in this component
 })
 
 export class CoursesComponent{
-
+    //define a field to hold title of the page
+    title = "List of courses";
+    
+    //define a method to get some value
+    getTitle(){
+        return this.title;
+    }
 }
